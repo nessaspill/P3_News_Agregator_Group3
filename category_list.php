606e282ab784cced753bd9ea70dedeb1c84ade83
nessaspill <vanessa.spillari@seattlecentral.edu>
2017-06-09 01:58:18 -0700

    SQL statement
    Issue with SQL statement solved

--- category_list.php
+++ category_list.php
@@ -1,31 +1,32 @@
 <?php
 /**
  * demo_list.php along with demo_view.php provides a sample web application
- * 
- * @package nmListView
- * @author Bill Newman <williamnewman@gmail.com>
+ *
  * @version 2.10 2012/02/28
  * @link http://www.newmanix.com/
  * @license http://opensource.org/licenses/osl-3.0.php Open Software License ("OSL") v. 3.0
- * @see demo_view.php
  * @todo none
+ * @author Bill Newman <williamnewman@gmail.com>
+ * @package nmListView
+ * @see demo_view.php
  */
 
-# '../' works for a sub-folder.  use './' for the root  
-require '../inc_0700/config_inc.php'; #provides configuration, pathing, error handling, db credentials 
- 
-# SQL statement
-$sql = "SELECT c.CategoryID, c.Feed_ID, f.Feed_ID, c.Title FROM feed_categories c LEFT JOIN feed_feeds f ON f.Feed_ID = c.Feed_ID";
+
+// '../' works for a sub-folder.  use './' for the root
+require '../inc_0700/config_inc.php'; //provides configuration, pathing, error handling, db credentials
+
+// SQL statement
+$sql = "SELECT c.CategoryID, c.FeedID, f.FeedID, c.Title FROM feed_categories c LEFT JOIN feed_details f ON f.FeedID = c.FeedID";
 
 
 
 
 
 
-#Fills <title> tag. If left empty will default to $PageTitle in config_inc.php  
+//Fills <title> tag. If left empty will default to $PageTitle in config_inc.php
 $config->titleTag = 'Feeds made with love & PHP in Seattle';
 
-#Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
+//Fills <meta> tags.  Currently we're adding to the existing meta tags in config_inc.php
 $config->metaDescription = 'Seattle Central\'s ITC250 Class Feeds are made with pure PHP! ' . $config->metaDescription;
 $config->metaKeywords = 'Muffins,PHP,Fun,Bran,Regular,Regular Expressions,'. $config->metaKeywords;
 
@@ -42,30 +43,28 @@ $config->nav1["page.php"] = "New Page!"; #add a new page to end of nav1 (viewabl
 $config->nav1 = array("page.php"=>"New Page!") + $config->nav1; #add a new page to beginning of nav1 (viewable this page only)!!
 */
 
-# END CONFIG AREA ---------------------------------------------------------- 
+// END CONFIG AREA ----------------------------------------------------------
 
-get_header(); #defaults to theme header or header_inc.php
+get_header(); //defaults to theme header or header_inc.php
 ?>
-<h3 align="center"><?=smartTitle();?></h3>
+<h3 align="center"><?php echo smartTitle();?></h3>
 
 
 <?php
 
-# connection comes first in mysqli (improved) function
-$result = mysqli_query(IDB::conn(),$sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
+// connection comes first in mysqli (improved) function
+$result = mysqli_query(IDB::conn(), $sql) or die(trigger_error(mysqli_error(IDB::conn()), E_USER_ERROR));
 
-if(mysqli_num_rows($result) > 0)
-{#records exist - process
-	while($row = mysqli_fetch_assoc($result))
-	{# process each row
-         echo '<div align="center"><a href="' . VIRTUAL_PATH . 'p3/category_view.php?id=' . (int)$row['CategoryID'] . '">' . dbOut($row['Title']) . '</a>';
-         echo '</div>';
- 
-	} 
-}else{#no records
-    echo "<div align=center>What! No feeds?  There must be a mistake!!</div>";	
+if (mysqli_num_rows($result) > 0) {//records exist - process
+	while ($row = mysqli_fetch_assoc($result)) {// process each row
+		echo '<div align="center"><a href="' . VIRTUAL_PATH . 'p3/category_view.php?id=' . (int)$row['CategoryID'] . '">' . dbOut($row['Title']) . '</a>';
+		echo '</div>';
+
+	}
+}else {//no records
+	echo "<div align=center>What! No feeds?  There must be a mistake!!</div>";
 }
 @mysqli_free_result($result);
 
-get_footer(); #defaults to theme footer or footer_inc.php
+get_footer(); //defaults to theme footer or footer_inc.php
 ?>
