c62f76a7f00ef4d5f8e011e7fa62ef6b44153a40
Erin R Martin <erinrmartin@yahoo.com>
2017-06-08 19:14:27 -0700

    Updates Databse File
    You will need to add the foreign key manually in adminer unless you can figure out what's wrong. Then again you may not have an issue. If you are able to add everything in the SQL command go for it. SQL is funky sometimes. The good thing is we have a work around.

--- category-tables.sql
+++ category-tables.sql
@@ -1,25 +1,14 @@
 #category-tables.sql
 
 DROP TABLE IF EXISTS feed_category_music;
-
-CREATE TABLE feed_category_music (
-CategoryID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-Title VARCHAR(255) DEFAULT '',
-Description TEXT DEFAULT '',
-Link TEXT DEFAULT '',
-DateAdded DATETIME,
-PRIMARY KEY (CategoryID)
-)ENGINE=INNODB; 
-
-INSERT INTO feed_category_music VALUES (NULL,'Indie Music','Music from Indie artists','https://news.google.com/news?q=music_indie&output=rss', NOW());
-INSERT INTO feed_category_music VALUES (NULL,'EDM','Music from electronic dance artists','https://news.google.com/news?q=music_edm&output=rss', NOW());
-INSERT INTO feed_category_music VALUES (NULL,'Hip-Hop','Music from Hip-Hop artists','https://news.google.com/news?q=music_hip-hop&output=rss', NOW());
-
-
+DROP TABLE IF EXISTS feed_category_travel;
 DROP TABLE IF EXISTS feed_category_sea_creatures;
 
-CREATE TABLE feed_category_sea_creatures (
+DROP TABLE IF EXISTS feed_category;
+
+CREATE TABLE feed_categories (
 CategoryID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+FeedID INT UNSIGNED DEFAULT 0,
 Title VARCHAR(255) DEFAULT '',
 Description TEXT DEFAULT '',
 Link TEXT DEFAULT '',
@@ -27,30 +16,12 @@ DateAdded DATETIME,
 PRIMARY KEY (CategoryID)
 )ENGINE=INNODB; 
 
-INSERT INTO feed_category_sea_creatures VALUES (NULL,'Seals','Semi-aquatic marine mammals','https://news.google.com/news?q=seals&output=rss', NOW());
-INSERT INTO feed_category_sea_creatures VALUES (NULL,'Narwhals','Medium-sized toothed whale that possesses a large "tusk" from a protruding canine tooth','https://news.google.com/news?q=narwhals&output=rss', NOW());
-INSERT INTO feed_category_sea_creatures VALUES (NULL,'Orcas','Aka Killer Whales, toothed whale belonging to the oceanic dolphin family','https://news.google.com/news?q=whales&output=rss', NOW());
-                                                
 
-DROP TABLE IF EXISTS feed_category_travel;
+DROP TABLE IF EXISTS feed_details;
 
-CREATE TABLE feed_category_travel (
+CREATE TABLE feed_details (
 CategoryID INT UNSIGNED NOT NULL AUTO_INCREMENT,
-Title VARCHAR(255) DEFAULT '',
-Description TEXT DEFAULT '',
-Link TEXT DEFAULT '',
-DateAdded DATETIME,
-PRIMARY KEY (CategoryID)
-)ENGINE=INNODB; 
-                                                
-INSERT INTO feed_category_travel VALUES (NULL,'Air Travel','Plane, Blimp/Zeppelin, Hot air balloon','https://news.google.com/news?q=travel_air&output=rss', NOW());  
-INSERT INTO feed_category_travel VALUES (NULL,'Sea','Curise ship, Yacht, Boat','https://news.google.com/news?q=travel_sea&output=rss', NOW()); 
-INSERT INTO feed_category_travel VALUES (NULL,'Land Travel','Automobile, Bus, Train, Bike','https://news.google.com/news?q=travel_land&output=rss', NOW()); 
-
-DROP TABLE IF EXISTS rss_feed_details;
-
-CREATE TABLE rss_feed_details (
-CategoryID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+FeedID INT UNSIGNED DEFAULT 0,
 Title VARCHAR(255) DEFAULT '',
 Description TEXT DEFAULT '',
 Link TEXT DEFAULT '',
@@ -60,5 +31,30 @@ Image_Url TEXT DEFAULT '',
 Image_Link TEXT DEFAULT '',
 Image_Width TEXT DEFAULT '',
 Image_Height TEXT DEFAULT '',
-PRIMARY KEY (CategoryID)
-)ENGINE=INNODB; 
+PRIMARY KEY (CategoryID),
+INDEX FeedID_index(FeedID),
+FOREIGN KEY (FeedID) REFERENCES feed_categories(FeedID) ON DELETE CASCADE
+)ENGINE=INNODB;
+
+
+
+INSERT INTO feed_categories VALUES (NULL,1,'Music-Indie','Music from Indie artists','https://news.google.com/news?q=music_indie&output=rss', NOW());
+INSERT INTO feed_categories VALUES (NULL,1,'Music-EDM','Music from electronic dance artists','https://news.google.com/news?q=music_edm&output=rss', NOW());
+INSERT INTO feed_categories VALUES (NULL,1,'Music-Hip-Hop','Music from Hip-Hop artists','https://news.google.com/news?q=music_hip-hop&output=rss', NOW());
+
+INSERT INTO feed_categories VALUES (NULL,2,'Travel-Air','Plane, Blimp/Zeppelin, Hot air balloon','https://news.google.com/news?q=travel_air&output=rss', NOW());  
+INSERT INTO feed_categories VALUES (NULL,2,'Travel-Sea','Curise ship, Yacht, Boat','https://news.google.com/news?q=travel_sea&output=rss', NOW()); 
+INSERT INTO feed_categories VALUES (NULL,2,'Travel-Land Travel','Automobile, Bus, Train, Bike','https://news.google.com/news?q=travel_land&output=rss', NOW()); 
+
+INSERT INTO feed_categories VALUES (NULL,3,'Sea_Creatures-Seals','Semi-aquatic marine mammals','https://news.google.com/news?q=seals&output=rss', NOW());
+INSERT INTO feed_categories VALUES (NULL,3,'Sea_Creatures-Narwhals','Medium-sized toothed whale that possesses a large "tusk" from a protruding canine tooth','https://news.google.com/news?q=narwhals&output=rss', NOW());
+
+INSERT INTO feed_categories VALUES (NULL,3,'Sea_Creatures-Orcas','Aka Killer Whales, toothed whale belonging to the oceanic dolphin family','https://news.google.com/news?q=whales&output=rss', NOW());
+                                                
+
+
+
+ 
+                                                
+
+
